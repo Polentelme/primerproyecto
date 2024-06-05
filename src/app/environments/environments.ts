112f7ee572096e1c699dcf80b25843af3c67e8c8
polentelme <aledinho29@gmail.com>
2024-06-05 11:09:58 -0300

    instalacion de firebase

---
+++ src/app/environments/environments.ts
@@ -0,0 +1,14 @@
+// configuracion de firebase 
+export const environment = {
+    production: false,
+
+        // sacamos la palabra "const" y el ";" final, cambiamos el "=" por ":"
+        firebaseConfig:{
+        apiKey: "AIzaSyCVEPlHlPmH3uKDioOD5UX4ux_59GEp7ws",
+        authDomain: "programacion-proyecto0.firebaseapp.com",
+        projectId: "programacion-proyecto0",
+        storageBucket: "programacion-proyecto0.appspot.com",
+        messagingSenderId: "129212422978",
+        appId: "1:129212422978:web:5ae3f45f9d3900ec7946db"
+      }
+}
