a02719fd2acdad1b49531b8f397d1bc7de2adaf7
zanahoria <lopezruiztomas59@gmail.com>
2024-08-20 10:58:31 -0300

    table component editar producto

--- src/app/modules/admin/components/table/table.component.ts
+++ src/app/modules/admin/components/table/table.component.ts
@@ -12,6 +12,10 @@ export class TableComponent {
   // Creamos colección local de productos -> la definimos como array
   coleccionProductos: Producto[] = [];
 
+  productoSeleccionado!: Producto; // ! <- tomar valores vacíos
+
+  modalVisibleProducto: boolean = false;
+
   // Definimos formulario para los productos
   /**
    * Atributos alfanuméricos (string) se inicializan con comillas simples
@@ -19,20 +23,26 @@ export class TableComponent {
    */
   producto = new FormGroup({
     nombre: new FormControl('', Validators.required),
-    precio: new FormControl(0,Validators.required),
+    precio: new FormControl(0, Validators.required),
     descripcion: new FormControl('', Validators.required),
     categoria: new FormControl('', Validators.required),
     imagen: new FormControl('', Validators.required),
     alt: new FormControl('', Validators.required)
   })
 
-  constructor(public servicioCrud: CrudService){}
-  ngOnInit(): void{}
+  constructor(public servicioCrud: CrudService) { }
 
-  async agregarProducto () {
-    if(this.producto.valid) {
-      let nuevoproducto: Producto = {
-        Idproducto '',
+  ngOnInit(): void {
+    // subscribe -> método de notificación de cambios (observable)
+    this.servicioCrud.obtenerProducto().subscribe(producto => {
+      this.coleccionProductos = producto;
+    })
+  }
+
+  async agregarProducto() {
+    if (this.producto.valid) {
+      let nuevoProducto: Producto = {
+        idProducto: '',
         nombre: this.producto.value.nombre!,
         precio: this.producto.value.precio!,
         descripcion: this.producto.value.descripcion!,
@@ -40,14 +50,58 @@ export class TableComponent {
         imagen: this.producto.value.imagen!,
         alt: this.producto.value.alt!
       }
-    }; 
-    await this.servicioCrud.crearProducto(nuevoProducto)
-    .then(producto => {
-      alert ("ha agregado un nuevo producto con exito.");
 
+      await this.servicioCrud.crearProducto(nuevoProducto)
+        .then(producto => {
+          alert("Ha agregado un nuevo producto con éxito.");
+        })
+        .catch(error => {
+          alert("Ha ocurrido un error al cargar un producto.");
+        })
+    }
+  }
+
+  // función vinculada al modal y el botón de la tabla
+  mostrarBorrar(productoSeleccionado: Producto){
+    this.modalVisibleProducto = true;
+
+    this.productoSeleccionado = productoSeleccionado;
+  }
+
+  borrarProducto(){
+    this.servicioCrud.eliminarProducto(this.productoSeleccionado.idProducto)
+    .then(respuesta => {
+      alert("Se ha podido eliminar con éxito.");
     })
     .catch(error => {
-      alert ("ha ocurrido un error al cargar un producto");
+      alert("Ha ocurrido un error al eliminar un producto: \n"+error);
     })
   }
+  // editar productos
+  editarProducto () {
+    let datos: Producto = {
+      //solo idProducto no se modifica por el usuario
+      Idproducto: this.productoSeleccionado.Idproducto,
+      /*los demas atributos reciben nueva informacion desde el 
+      formulario */ 
+      nombre: this.producto.value.nombre!,
+      precio: this.producto.value.precio!,
+      descripcion: this.producto.value.descripcion!,
+      imagen: this.producto.value.imagen!,
+      alt: this.producto.value.alt!
+
+    }
+
+    this.servicioCrud.modificarProducto(this.productoSeleccionado.Idproducto, datos)
+    .then(producto =>{
+      alert("el producto se ha modificado con exito.");
+    })
+    .catch (error => {
+   alert ("hubo un problema al modificar el producto: ")
+    });
+    
+  }
+  
 }
+// editar productos
+
